d1caf099f7745f6de3c925ead32361dd12886c59
Marco Tazzari <mtazzari@gmail.com>
2023-08-14 02:08:01 +0100

    add logging and polish cli arguments

--- genswaptrades/cli.py
+++ genswaptrades/cli.py
@@ -4,6 +4,7 @@
 # command line interface (CLI) definition
 #
 import argparse
+import logging
 import os
 from glob import glob
 
@@ -11,34 +12,47 @@ from genswaptrades import __version__
 from genswaptrades.tests import TEST_ASSETS_DIR
 from genswaptrades.trades import generate_trades, print_trades
 
+# set up the root_logger
+root_logger = logging.getLogger('')
+
 
 def parse_args() -> dict:
     """Parse arguments from CLI.
-
 
     Returns:
         dict: the parsed arguments.
     """
     parser = argparse.ArgumentParser(
-        description="A Python command-line utility that computes premiums for specific exposure models.",
+        description="A Python command-line utility that, for a given list of trades, generates the 1 or 2 interest rate swap trades\nneeded to achieve zero-sum notional value and cashflow.",
         usage='use "%(prog)s --help" for more information',
         formatter_class=argparse.RawTextHelpFormatter  # for multi-line help text
     )
-    parser.add_argument(action="store",
-                        help="[str] path to JSON file containing definition of model, parameters, and outputs.",
+    parser.add_argument("filename",
+                        action="store",
+                        help="[str] path to csv file containing the trades (columns: notional, rate).\n",
                         type=str,
-                        default="",
-                        dest="filename")
+                        nargs="?",  # make the positional argument optional
+                        default="")
     parser.add_argument("--run-tests", action="store_true",
-                        help="[bool] if True, generate interest swap trades for all the test cases. Default: False",
+                        help="[bool] if True, generate interest swap trades for all the 3 provided test cases. Default: False",
                         default=False)
-    parser.add_argument("-d", "--debug", action="store_true",
-                        help="[bool] if True, the full model_data dict with results is printed to terminal. Default: False",
-                        default=False)
+    parser.add_argument("--min-rate", action="store",
+                        type=float,
+                        default=-0.1,
+                        help="[float] minimum allowed rate. Default: -0.1."),
+    parser.add_argument("--max-rate", action="store",
+                        type=float,
+                        default=0.1,
+                        help="[float] maximum allowed rate. Default: 0.1."),
     parser.add_argument("--rates", action="store",
                         type=float, nargs=2,
-                        help="[list of floats] terminal. Default: False",
-                        )
+                        help="[list of floats] if 2 trades are necessary to achieve zero-sum, the rates of these 2 trades.\n"
+                             "The rates must be different and within [min_rate, max_rate] interval.\n"
+                             "Example: `--rates 0.3 0.1`.\n"
+                             "Default: None, which is equivalent to using rates [max_rate, max_rate - 0.1 * (max_rate - min_rate)]")
+    parser.add_argument('--log-level',
+                        help='log level (debug:10, info:20, warning:30, error:40, critical:50). Default: 30',
+                        default=30, type=int)
     parser.add_argument("-V", "--version",
                         action="version",
                         version='{}'.format(__version__)
@@ -58,9 +72,9 @@ def validate_args(args: dict) -> None:
     Raises:
         ValueError: if the args['filename'] is not a csv file.
     """
-    _, ext = os.path.splitext(args['filename'])
+    filename, ext = os.path.splitext(args['filename'])
 
-    if ext.upper() != '.CSV':
+    if filename and ext.upper() != '.CSV':
         raise ValueError(f"Expecting the filename with 'CSV' or 'csv' extension, got '{ext}'.")
 
 
@@ -69,19 +83,46 @@ def main() -> None:
     Main function, called through the shell entrypoint.
     Parse the arguments, read the input csv filename, and generate the trades.
     """
+    # parse and validate the arguments
     args = parse_args()
-
     validate_args(args)
 
+    # setup logger
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root_logger.addHandler(ch)
+    logging_level = args.pop('log_level')
+    root_logger.setLevel(logging_level)
+
+    # run the trade generator
+    root_logger.debug("command-line arguments: {}".format(args))
     if args['run_tests']:
         test_files = glob(os.path.join(TEST_ASSETS_DIR, "trades*.csv"))
-
+        root_logger.info("The following test files were found:\n{}\n".format("\n".join(test_files)))
+        root_logger.info("Starting the trade generation:")
         for test_file in test_files:
             print(test_file)
-            print_trades(generate_trades(test_file))
+            print_trades(
+                generate_trades(
+                    test_file,
+                    min_rate=args['min_rate'],
+                    max_rate=args['max_rate'],
+                    additional_trade_rates=args.get('rates')
+                )
+            )
+
+        exit(0)
 
     # generate and print the trades
-    print_trades(generate_trades(args['filename'], additional_trade_rates=args.get('rates')))
+    print_trades(
+        generate_trades(
+            args['filename'],
+            min_rate=args['min_rate'],
+            max_rate=args['max_rate'],
+            additional_trade_rates=args.get('rates')
+        )
+    )
 
 
 if __name__ == "__main__":
--- genswaptrades/trades.py
+++ genswaptrades/trades.py
@@ -3,11 +3,12 @@
 #
 # trades functionality
 #
+import logging
+
 import numpy as np
 import pandas as pd
 
-# todo add logging.
-# todo add docs on linear system of equation
+logger = logging.getLogger(__name__)
 
 
 def generate_trades(fname: str,
@@ -29,8 +30,8 @@ def generate_trades(fname: str,
         fname (str): Filename containing the trades.
         min_rate (float, optional): minimum allowed trade rate. Defaults to -0.1.
         max_rate (float, optional): maximum allowed trade rate. Defaults to 0.1.
-        additional_trade_rates (list[float], optional): rates for the 2 trades necessary to achieve zero-sum. Defaults to None.
-            If None, they default to [0.1, 0.05]. They must be different and can take any float value within [min_rate, max_rate].
+        additional_trade_rates (list[float], optional): if 2 trades are necessary to achieve zero-sum, the rates of these two trades. Defaults to None.
+            If None, they default to [max_rate, max_rate - 0.1 * (max_rate - min_rate)]. They must be different and can take any float value within [min_rate, max_rate].
 
     Returns:
         list[tuple[int, float, float, float]]: list of tuples with all the generated trades,
@@ -38,12 +39,12 @@ def generate_trades(fname: str,
     """
     if not additional_trade_rates:
         # set additional_trade_rates dynamically (avoid mutable default argument value)
-        additional_trade_rates = [0.1, 0.05]
+        additional_trade_rates = [max_rate, max_rate - 0.1 * (max_rate - min_rate)]
 
     # check that the additional_trade_rates are within [min_rate, max_rate] interval
     for rate in additional_trade_rates:
         assert min_rate <= rate <= max_rate, \
-            f"Expect additional_trade_rate={rate} to be in [{min_rate},{max_rate}]"
+            f"Expect additional_trade_rate to be in [{min_rate},{max_rate}], got {rate}."
 
     # read trades and compute total notional and cashflow, after rounding them.
     df = pd.read_csv(fname)
@@ -55,15 +56,17 @@ def generate_trades(fname: str,
 
     new_trades = []
 
-    verbose_txt = ""
-    verbose_txt += f"{Ntrades} trades have been imported from {fname}\n"
-    verbose_txt += f"Existing trades notional_sum={notional_rounded_sum} and cashflow_sum={cashflow_rounded_sum}\n"
+    logger.info(f"Starting the trade generation for input file {fname}: {Ntrades} trades found.")
+    logger.info(f"Initial notional_sum={notional_rounded_sum} and cashflow_sum={cashflow_rounded_sum}.")
 
     # compute the rate for the next trade to achieve zero-sum notional and cashflow.
     next_trade_rate = cashflow_rounded_sum / notional_rounded_sum
+    logger.info(f"The rate of a single trade needed to achieve zero-sum is {next_trade_rate:11.8f}")
 
     if next_trade_rate < min_rate or next_trade_rate > max_rate:
         # more than 1 trade is necessary, ensure that we can make it in 2 trades
+        logger.info(f"The single-trade rate is outside of [{min_rate:11.8f}, {max_rate:11.8f}] interval: 2 trades are necessary.")
+
         trade_1_rate, trade_2_rate = additional_trade_rates
 
         # the first trade is done at trade_1_rate
@@ -84,15 +87,16 @@ def generate_trades(fname: str,
         new_trades.append(
             (Ntrades, trade_2_notional, trade_2_rate, trade_2_cashflow)
         )
-        verbose_txt += "Number of generated trades: 2\n"
 
     else:
         # 1 trade is sufficient as next_rate is within [min_rate, max_rate].
+        logger.info("The single-trade rate is within of [min_rate, max_rate] interval: 1 trade is sufficient.")
+
         Ntrades += 1
         new_trades.append(
             (Ntrades, -notional_rounded_sum, np.round(next_trade_rate, 8), -cashflow_rounded_sum)
         )
-        verbose_txt += "Number of generated trades: 1\n"
+        logger.info("Number of generated trades: 1")
 
     for new_trade in new_trades:
         notional_rounded_sum += np.round(new_trade[1], 2)
@@ -102,9 +106,9 @@ def generate_trades(fname: str,
     assert np.abs(notional_rounded_sum) < 0.01
     assert np.abs(cashflow_rounded_sum) < 0.01
 
-    # log trades notional_sum = {notional_rounded_sum} and cashflow_sum = {cashflow_rounded_sum}"
-
-    # trades_txt = "\n".join(["Trade {}   {:15.2f}  {:11.8f}  {:15.2f}".format(*new_trade) for new_trade in new_trades])
+    logger.info(
+        f"Final notional_sum and cashflow_sum are expected to be both zero (i.e. < 0.01), got notional_sum={notional_rounded_sum:<15.2f} and cashflow_sum={cashflow_rounded_sum:<15.2f}")
+    logger.info(f"Output columns, 1 row for each trade: Trade <trade no.>  <notional value>  <rate>  <cashflow>")
 
     return new_trades
 
